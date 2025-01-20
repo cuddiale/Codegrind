8359708fa488617a98cf5899dab3e66a93f1e2c6
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-20 09:51:15 +0100

    create

---
+++ linguaggi di programmazione/python/variabili/es.7.py
@@ -0,0 +1,14 @@
+# Creare delle variabili "nome", "cognome" ed "anno_di_nascita" ed assegnarle il valore dei valori. Mandare a schermo in un unico print. Sovrascrivere tutte le variabili e rimandare a schermo una seconda volta.
+
+nome = "Mario"
+cognome = "Rossi"
+anno_di_nascita = 2000
+print(nome, cognome, anno_di_nascita)
+
+
+nome = "Luigi"
+cognome = "Bianchi"
+anno_di_nascita = 1990
+print(nome, cognome, anno_di_nascita)
+
+# Output:
