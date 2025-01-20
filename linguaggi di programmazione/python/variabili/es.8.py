86b81c2065f07b2c69055abca452e53099b40d4c
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-20 09:52:26 +0100

    update

---
+++ linguaggi di programmazione/python/variabili/es.8.py
@@ -0,0 +1,8 @@
+# Creare una variabile `eta_attuale` e assegnargli il valore dell'età che hai attualmente, calcolandola in base all'anno corrente. Mandare a schermo `eta_attuale`
+
+anno_corrente = 2025
+anno_nascita = 1999
+eta_attuale = anno_corrente - anno_nascita
+print(eta_attuale)
+
+# Output: 
