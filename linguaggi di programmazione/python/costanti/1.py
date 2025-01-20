670733e44b88cef20acf073e09ab97f7213c47f6
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-20 09:56:58 +0100

    update

---
+++ linguaggi di programmazione/python/costanti/1.py
@@ -0,0 +1,7 @@
+# Definire una costante per il valore di pi greco e usarla per calcolare la circonferenza di un cerchio di raggio 5.
+
+PI_GRECO = 3.14159
+raggio = 5
+circonferenza = 2 * PI_GRECO * raggio
+
+print("La circonferenza del cerchio è:", circonferenza)
