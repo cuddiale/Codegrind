cfc41dfdb701726635bceaacfc4e03307aef25aa
cuddiale <alessiocuddretto@hotmail.com>
2025-01-20 14:05:35 +0100

    update

---
+++ linguaggi di programmazione/python/costanti/3.py
@@ -0,0 +1,7 @@
+# Definire una costante per il tasso di cambio euro-dollaro e usarla per convertire 100 euro in dollari.
+
+TASSO_CAMBIO_EURO_DOLLARO = 1.18
+euro = 100
+dollari = euro * TASSO_CAMBIO_EURO_DOLLARO
+
+print("100 euro corrispondono a", dollari, "dollari")
---
+++ linguaggi di programmazione/python/costanti/4.py
@@ -0,0 +1,7 @@
+# Definire una costante per la velocità della luce e usarla per calcolare quanto tempo impiega la luce a percorrere 1.000.000 km.
+
+VELOCITA_LUCE = 299792458
+distanza = 1000000
+tempo = distanza / VELOCITA_LUCE
+
+print("La luce impiega", tempo, "secondi a percorrere 1.000.000 km")
