78a727ec2f60e5c549822c05733f8575614baf84
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-20 09:47:01 +0100

    update

---
+++ linguaggi di programmazione/python/variabili/es.6.py
@@ -0,0 +1,5 @@
+# Creare una variabile "eta_futura" e assegnargli il valore dell'età che avrai tra 10 anni (utilizzando la variabile età già esistente). Mandare a schermo.
+
+eta = 20
+eta_futura = eta + 10
+print(eta_futura)
