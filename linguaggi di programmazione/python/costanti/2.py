b3271ee0a9c6bc0caf88453be9954cc7599f772c
cuddiale <alessiocuddretto@hotmail.com>
2025-01-20 13:47:56 +0100

    update

---
+++ linguaggi di programmazione/python/costanti/2.py
@@ -0,0 +1,7 @@
+# Definire una costante per il valore della gravità terrestre e usarla per calcolare il peso di un oggetto di massa 10 kg.
+
+GRAVITA_TERRESTRE = 9.81
+massa = 10
+peso = massa * GRAVITA_TERRESTRE
+
+print("Il peso dell'oggetto è:", peso)
