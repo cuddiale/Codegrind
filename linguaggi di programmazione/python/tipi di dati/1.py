34ac5279130f56e9cc86b99997e595464816d579
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-21 11:34:12 +0100

    update

--- linguaggi di programmazione/python/tipi di dati/1.py
+++ linguaggi di programmazione/python/tipi di dati/1.py
@@ -1,1 +1,5 @@
 # Dichiarare una variabile "numero_intero" e assegnargli un valore **intero**. Mandare a schermo il tipo della variabile per conferma.
+
+numero_intero = 5
+print(type(numero_intero))
+
