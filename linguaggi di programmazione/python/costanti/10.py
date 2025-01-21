e7c5f8c25845559d1c3e1575fafc02645e755b26
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-21 09:50:23 +0100

    update

---
+++ linguaggi di programmazione/python/costanti/10.py
@@ -0,0 +1,7 @@
+# Definire una costante per il numero di centimetri in un metro e usarla per convertire 250 centimetri in metri.
+
+CENTIMETRI_METRO = 100
+centimetri = 250
+metri = centimetri / CENTIMETRI_METRO
+
+print("250 centimetri sono", metri, "metri")
---
+++ linguaggi di programmazione/python/costanti/9.py
@@ -0,0 +1,7 @@
+# Definire una costante per il numero di secondi in un'ora e usarla per convertire 5 ore in secondi.
+
+SECONDI_ORA = 3600
+ore = 5
+secondi = ore * SECONDI_ORA
+
+print("In 5 ore ci sono", secondi, "secondi")
