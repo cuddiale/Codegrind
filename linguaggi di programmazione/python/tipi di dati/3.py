10c40c56f5dda619003d19f4c043f49eca9f145e
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-21 11:40:49 +0100

    update

---
+++ linguaggi di programmazione/python/tipi di dati/3.py
@@ -0,0 +1,4 @@
+# Creare una variabile "testo" e assegnargli una **stringa** contenente una frase. Mandare a schermo il tipo della variabile per conferma.
+
+testo = "Ciao, come stai?"
+print(type(testo))
---
+++ linguaggi di programmazione/python/tipi di dati/4.py
@@ -0,0 +1,4 @@
+# Creare una variabile "valore_booleano" e assegnargli un valore **booleano**. Mandare a schermo il tipo della variabile per conferma.
+
+valore_booleano = True
+print(type(valore_booleano))
