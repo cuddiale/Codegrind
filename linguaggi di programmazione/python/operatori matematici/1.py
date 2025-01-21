cb94aa32efe33db106fffff6af25c5a3f7577d3e
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-21 11:43:49 +0100

    update

---
+++ linguaggi di programmazione/python/operatori matematici/1.py
@@ -0,0 +1,7 @@
+# Dichiarare due variabili "numero1" e "numero2" e assegnargli due numeri interi. Eseguire l'addizione tra le due variabili e assegnare il risultato ad una nuova variabile "somma", mandandola a schermo.
+
+numero1 = 5
+numero2 = 3
+somma = numero1 + numero2
+print(somma)
+
