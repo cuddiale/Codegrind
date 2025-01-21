0a7aa580c086113829881e8dba356b082e746248
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-21 09:49:18 +0100

    create

---
+++ linguaggi di programmazione/python/costanti/8.py
@@ -0,0 +1,7 @@
+# Definire una costante per il valore di Planck e usarla per calcolare l'energia di un fotone con una frequenza di 5e14 Hz.
+
+h = 6.62607015e-34
+f = 5e14
+E = h * f
+
+print("L'energia di un fotone con frequenza", f, "Hz è", E, "J")
