cb7cc388e81750f0ce0b38bc8cb87095886a40b4
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-21 11:35:02 +0100

    update

---
+++ linguaggi di programmazione/python/tipi di dati/2.py
@@ -0,0 +1,4 @@
+# Dichiarare una variabile "numero_decimale" e assegnargli un valore **float**. Mandare a schermo il tipo della variabile per conferma.
+
+numero_decimale = 5.0
+print(type(numero_decimale))
