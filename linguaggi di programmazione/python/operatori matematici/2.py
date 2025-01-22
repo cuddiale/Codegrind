ccb0b48656c90da6420065ccd5ace55b93c636f6
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-22 12:09:08 +0100

    update

---
+++ linguaggi di programmazione/python/operatori matematici/2.py
@@ -0,0 +1,7 @@
+# Eseguire la sottrazione tra le due variabili e assegnare il risultato ad una nuova variabile "differenza". Mandare a schermo.
+
+numero1 = 5
+numero2 = 3
+differenza = numero1 - numero2
+
+print(differenza)
---
+++ linguaggi di programmazione/python/operatori matematici/3.py
@@ -0,0 +1,7 @@
+# Eseguire la moltiplicazione tra le due variabili e assegnare il risultato ad una nuova variabile "prodotto". Mandare a schermo.
+
+a = 5
+b = 3
+prodotto = a * b
+
+print(prodotto)
