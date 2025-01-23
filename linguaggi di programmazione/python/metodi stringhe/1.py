470ae9213dc9bcccf5da8e5caa19c6ce46517193
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-23 11:25:46 +0100

    create

---
+++ linguaggi di programmazione/python/metodi stringhe/1.py
@@ -0,0 +1,5 @@
+# Assegnare una stringa "ciao mondo" ad una variabile "stringa" e utilizzare il metodo upper() per convertirla in maiuscolo in una nuova variabile.
+
+stringa = "ciao mondo"
+stringa_maiuscola = stringa.upper()
+print(stringa_maiuscola)
