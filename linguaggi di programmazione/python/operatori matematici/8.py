0091dcc5cc161c4320baf99e35416d9cdd0621b6
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-23 11:21:24 +0100

    update

---
+++ linguaggi di programmazione/python/operatori matematici/8.py
@@ -0,0 +1,6 @@
+# Trovare quella operazione per portare "numero1" a valere 1 senza riassegnarlo direttamente ad 1 e sottraendolo a se stesso.
+
+numero1 = 10
+numero1 = numero1 - (numero1 - 1)
+
+print(numero1)
