91426d6ac0e752b9598d15656ce14c82821dc702
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-23 11:32:23 +0100

    updatae

---
+++ linguaggi di programmazione/python/metodi stringhe/2.py
@@ -0,0 +1,6 @@
+# Assegnare una stringa "Benvenuti a Roma" ad una variabile "stringa" e utilizzare il metodo lower() per convertirla in minuscolo in una nuova variabile.
+
+stringa = "Benvenuti a Roma"
+stringa_minuscola = stringa.lower()
+
+print(stringa_minuscola)
---
+++ linguaggi di programmazione/python/metodi stringhe/3.py
@@ -0,0 +1,5 @@
+# Assegnare una stringa "Il meglio deve ancora venire" ad una variabile "stringa" e utilizzare il metodo split() per dividere la stringa in una lista di parole.
+
+stringa = "Il meglio deve ancora venire"
+lista = stringa.split()
+print(lista)
