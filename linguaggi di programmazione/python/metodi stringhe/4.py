d4ef2067704eae4aee0686a8bbc4d29aaaa876f1
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-27 10:01:22 +0100

    create

---
+++ linguaggi di programmazione/python/metodi stringhe/4.py
@@ -0,0 +1,6 @@
+# Assegnare una stringa "Hello World" ad una variabile "stringa" e utilizzare il metodo replace() per sostituire "World" con "Python".
+
+stringa = "Hello World"
+stringa = stringa.replace("World", "Python")
+
+print(stringa)
