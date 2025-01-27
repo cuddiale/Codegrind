bc93828f3f3f236a728059ed5b92ca37ad1db947
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-27 10:02:00 +0100

    update

---
+++ linguaggi di programmazione/python/metodi stringhe/5.py
@@ -0,0 +1,6 @@
+# Assegnare una stringa " Ciao " ad una variabile "stringa" e utilizzare il metodo strip() per rimuovere gli spazi vuoti all'inizio e alla fine della stringa..
+
+stringa = "    Ciao      "
+stringa_modificata = stringa.strip()
+
+print(stringa_modificata)
