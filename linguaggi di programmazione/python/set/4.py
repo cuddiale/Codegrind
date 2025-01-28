b39ab13d0d142400b5e21226dac3f09e4e5484af
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-28 17:16:49 +0100

    update

---
+++ linguaggi di programmazione/python/set/4.py
@@ -0,0 +1,6 @@
+# Rimuovere l'elemento "mela" dal set precedente.
+
+frutta = {"mela", "banana", "ciliegia", "kiwi", "ananas"}
+frutta.remove("mela")
+
+print(frutta)
---
+++ linguaggi di programmazione/python/set/5.py
@@ -0,0 +1,9 @@
+# Verificare se l'elemento "ananas" è presente nel set precedente.
+
+frutta = {"mela", "banana", "ciliegia", "ananas", "kiwi"}
+
+if "ananas" in frutta:
+    print("L'elemento ananas è presente nel set")
+else:
+    print("L'elemento ananas non è presente nel set")
+
