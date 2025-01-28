fcf2a8ef7ed8bd1c0c2ded53858064cdaa27b73f
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-28 17:48:34 +0100

    update

---
+++ linguaggi di programmazione/python/condizioni if/3.py
@@ -0,0 +1,9 @@
+# Scrivere un programma che chiede all'utente di inserire una stringa e stampa "La stringa è vuota" se la stringa è vuota, altrimenti stampa "La stringa non è vuota".
+
+stringa = input("Inserisci una stringa: ")
+
+stringa = input("Inserisci una stringa: ")
+if not stringa:
+    print("La stringa è vuota")
+else:
+    print("La stringa non è vuota")
