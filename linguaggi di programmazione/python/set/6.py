484fc385a6d3002cb3add2f0abec43b0e4a745ac
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-28 17:18:31 +0100

    update

---
+++ linguaggi di programmazione/python/set/6.py
@@ -0,0 +1,5 @@
+# Creare un nuovo set contenente gli elementi "banana", "kiwi" e "arancia".
+
+frutta = {"mela", "banana", "kiwi", "arancia"}
+print(frutta)
+
---
+++ linguaggi di programmazione/python/set/7.py
@@ -0,0 +1,6 @@
+# Creare un set contenente i numeri interi da 1 a 5 ricavati da un range().
+
+numeri = set(range(1, 6))
+
+print(numeri)
+
