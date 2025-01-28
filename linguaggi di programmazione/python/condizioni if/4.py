b5cdaba7baf35cd7ccf21afe24297bb028ef481e
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-28 17:49:13 +0100

    update

---
+++ linguaggi di programmazione/python/condizioni if/4.py
@@ -0,0 +1,11 @@
+# Scrivere un programma che chiede all'utente di inserire un numero e stampa "Il numero è pari" se il numero è pari, altrimenti stampa "Il numero è dispari".
+
+numero = int(input("Inserisci un numero: "))
+
+if numero % 2 == 0:
+    print("Il numero è pari")
+
+else:
+    print("Il numero è dispari")
+
+    
