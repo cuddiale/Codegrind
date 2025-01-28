46434c53342abf563006d5bc72c251a865df044e
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-28 10:17:54 +0100

    update

---
+++ linguaggi di programmazione/python/set/2.py
@@ -0,0 +1,4 @@
+# Creare un set contenente i seguenti elementi: "mela", "banana", "kiwi", "mela".
+
+s = {"mela", "banana", "kiwi", "mela"}
+print(s)
---
+++ linguaggi di programmazione/python/set/3.py
@@ -0,0 +1,6 @@
+# Aggiungere l'elemento "pesca" al set precedente.
+
+frutta = {"mela", "banana", "ciliegia", "kiwi", "ananas"}
+frutta.add("pesca")
+
+print(frutta)
