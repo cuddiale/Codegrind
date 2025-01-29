4a660c5aae5c04aca786075ec00d85eaaaace99d
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-29 11:09:33 +0100

    update

---
+++ linguaggi di programmazione/python/cicli for/5.py
@@ -0,0 +1,5 @@
+# Scrivere un programma che utilizzi un loop for per stampare tutte le lettere di una stringa.
+stringa = "ciao"
+
+for i in stringa:
+    print(i)
---
+++ linguaggi di programmazione/python/cicli for/6.py
@@ -0,0 +1,7 @@
+# Scrivere un programma che utilizzi un loop for per stampare tutte le chiavi di un dizionario.
+
+d = {'a': 1, 'b': 2, 'c': 3}
+
+for k in d:
+    print(k)
+
