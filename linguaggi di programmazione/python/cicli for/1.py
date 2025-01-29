1c5166af957ae6d8ab2cc3e463b82985ba120ed2
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-29 10:47:06 +0100

    update

---
+++ linguaggi di programmazione/python/cicli for/1.py
@@ -0,0 +1,6 @@
+# Scrivere un programma che utilizzi un loop for per stampare ogni elemento di una lista.
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in lista:
+    print(i)
---
+++ linguaggi di programmazione/python/cicli for/2.py
@@ -0,0 +1,5 @@
+# Scrivere un programma che utilizzi un loop for per stampare tutti i numeri da 1 a 10.
+
+for i in range(1, 11):
+    print(i)
+
