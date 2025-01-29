48bba0b7026cdb87df1a4216a26148c674ffcc88
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-29 11:03:30 +0100

    update

---
+++ linguaggi di programmazione/python/cicli for/3.py
@@ -0,0 +1,9 @@
+# Scrivere un programma che utilizzi un loop for per sommare tutti i numeri in una lista.
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+sum = 0
+for i in numbers:
+    sum += i
+
+print(sum)
---
+++ linguaggi di programmazione/python/cicli for/4.py
@@ -0,0 +1,4 @@
+# Scrivere un programma che utilizzi un loop for per stampare tutti i numeri pari da 1 a 20.
+
+for i in range(2, 21, 2):
+    print(i)
