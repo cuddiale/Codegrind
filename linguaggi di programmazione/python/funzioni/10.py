997bce870ba3cc0085857eba6ae96822e047d02d
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-29 18:30:09 +0100

    update

--- linguaggi di programmazione/python/funzioni/10.py
+++ linguaggi di programmazione/python/funzioni/10.py
@@ -1,2 +1,8 @@
 # Scrivi una funzione che prende una lista di numeri e restituisce una lista contenente solo i numeri maggiori di un valore specificato.
 
+def numeri_maggiori_di(lista, valore):
+    risultato = []
+    for numero in lista:
+        if numero > valore:
+            risultato.append(numero)
+    return risultato
