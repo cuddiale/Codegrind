6c6548c3910de328bd5a8b5ae81e2fbacf2eaf4d
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-30 11:02:35 +0100

    update

---
+++ linguaggi di programmazione/python/moduli/4.py
@@ -0,0 +1,5 @@
+# Crea un nuovo file Python chiamato altro_modulo.py e definisci una variabile chiamata lista_numeri che contiene una lista di numeri interi. Poi importa il modulo nel tuo programma principale e stampa la lista.
+
+import altro_modulo
+
+print(altro_modulo.lista_numeri)
