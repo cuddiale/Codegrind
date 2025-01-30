552f290a2b75499787ab190812afd79844fb40e1
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-01-30 10:02:09 +0100

    update

---
+++ linguaggi di programmazione/python/moduli/1.py
@@ -0,0 +1,8 @@
+# Importa il modulo math e calcola la radice quadrata di 16.
+
+import math
+numero= int(input("Inserisci un numero: "))
+radice = math.sqrt(numero)
+
+print(f"La radice quadrata di {numero} è {radice}")
+
---
+++ linguaggi di programmazione/python/moduli/2.py
@@ -0,0 +1,7 @@
+# Importa il modulo random e genera un numero casuale compreso tra 1 e 10..
+
+import random
+
+random_number = random.randint(1, 10)
+
+print(f"Numero casuale: {random_number}")
