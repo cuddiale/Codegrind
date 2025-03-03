f25d74c11ba5b99f8c3ca7d6613e7318104a26fe
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-03-03 10:21:36 +0100

    update

---
+++ linguaggi di programmazione/JavaScript/Condizioni IF ELSE/1.js
@@ -0,0 +1,11 @@
+// Scrivi un programma che prenda un numero e stampi sulla console se è positivo, negativo o zero.
+
+const numero = 10;
+
+if (numero > 0) {
+  console.log("Il numero è positivo");
+} else if (numero < 0) {
+  console.log("Il numero è negativo");
+} else {
+  console.log("Il numero è zero");
+}
