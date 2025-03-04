76ba8b481212c3bf21d027b6ffc6bd8f6b665c05
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-03-04 12:30:07 +0100

    update

---
+++ linguaggi di programmazione/JavaScript/Ciclo For OF/1.js
@@ -0,0 +1,7 @@
+// Scrivi un programma che prenda un array di numeri e stampi ogni elemento.
+
+const numeri = [1, 2, 3, 4, 5];
+
+for (let numero of numeri) {
+  console.log(numero);
+}
---
+++ linguaggi di programmazione/JavaScript/Ciclo For OF/2.js
@@ -0,0 +1,7 @@
+// Scrivi un programma che prenda una stringa in input e stampi ogni lettera della stringa.
+
+const stringa = "Hello, world!";
+
+for (let lettera of stringa) {
+  console.log(lettera);
+}
