8da7059d2955e499e29b14b36ae1bacbada9c6a0
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-03-06 11:41:53 +0100

    update

--- linguaggi di programmazione/JavaScript/Parametri Rest/2.js
+++ linguaggi di programmazione/JavaScript/Parametri Rest/2.js
@@ -1,9 +1,8 @@
 // Creare una funzione che concatena un numero variabile di stringhe.
 
 function concatena(...stringhe) {
-    return stringhe.join(" ");
-  }
-  
-  console.log(concatena("Ciao", "mondo", "!")); // "Ciao mondo !"
-  console.log(concatena("JavaScript", "è", "fantastico")); // "JavaScript è fantastico"
-  Esercizio 3: Trovare il massimo
+  return stringhe.join(" ");
+}
+
+console.log(concatena("Ciao", "mondo", "!")); // "Ciao mondo !"
+console.log(concatena("JavaScript", "è", "fantastico")); // "JavaScript è fantastico"
