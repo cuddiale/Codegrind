760e7001c8e5c548921f9ecf7c1628eec99dc334
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-03-06 11:29:36 +0100

    update

---
+++ linguaggi di programmazione/JavaScript/Arrow Function/1.js
@@ -0,0 +1,5 @@
+// Scrivi una arrow function che prenda due parametri numerici e restituisca la loro somma.
+
+const somma = (a, b) => a + b;
+
+console.log("Somma:", somma(5, 3));
---
+++ linguaggi di programmazione/JavaScript/Arrow Function/2.js
@@ -0,0 +1,5 @@
+// Scrivi una arrow function che prenda un parametro numerico e restituisca il quadrato del numero.
+
+const quadrato = (numero) => numero ** 2;
+
+console.log("Quadrato:", quadrato(4));
