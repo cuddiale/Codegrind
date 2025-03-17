8b477408a68a9bf6c9a80ec6011024d4ca62f480
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-03-17 10:31:20 +0100

    update

---
+++ linguaggi di programmazione/Java/costanti/1.java
@@ -0,0 +1,9 @@
+// Definire una costante intera e stamparne il valore.
+
+public class CostantiEsempio {
+    public static final int COSTANTE_INTERA = 10;
+
+    public static void main(String[] args) {
+        System.out.println("Valore della costante: " + COSTANTE_INTERA);
+    }
+}
---
+++ linguaggi di programmazione/Java/costanti/2.java
@@ -0,0 +1,9 @@
+// Definire una costante di tipo stringa e stamparne il valore.
+
+public class CostantiStringa {
+    public static final String SALUTO = "Ciao, mondo!";
+
+    public static void main(String[] args) {
+        System.out.println("Saluto: " + SALUTO);
+    }
+}
