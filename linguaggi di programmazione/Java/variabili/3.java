e0c4f3b685c04dcfe63131b7edf0209619811866
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-03-17 10:05:26 +0100

    create

---
+++ linguaggi di programmazione/Java/variabili/3.java
@@ -0,0 +1,4 @@
+// Scrivi un programma Java che dichiara una variabile di tipo boolean chiamata "isPieno" e le assegna il valore true. Stampa il valore della variabile.
+
+boolean isPieno = true;
+System.out.println(isPieno);
