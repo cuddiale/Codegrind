4967744947a8638e2b86b1c8cda216ae122b8961
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-03-17 10:38:15 +0100

    update

---
+++ linguaggi di programmazione/Java/tipi di dati/1.java
@@ -0,0 +1,4 @@
+// Scrivi un programma Java che dichiara una variabile intera chiamata "numero" e le assegna il valore 10. Stampa il valore della variabile.
+
+int numero = 10;
+System.out.println(numero);
