e767c0a59be9370ccd135b524cc5e6792c0fb475
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-03-17 10:11:00 +0100

    update

---
+++ linguaggi di programmazione/Java/variabili/6.java
@@ -0,0 +1,4 @@
+// Scrivi un programma Java che dichiara una variabile di tipo double chiamata "peso" e le assegna il valore 68.5. Stampa il valore della variabile.
+
+double peso = 68.5;
+System.out.println(peso);
---
+++ linguaggi di programmazione/Java/variabili/9.java
@@ -0,0 +1,5 @@
+// Scrivi un programma Java che dichiara una variabile intera chiamata "annoDiNascita" e le assegna il valore 1990. Calcola l'età basandoti sull'anno corrente (supponi che sia il 2023) e stampa il risultato.
+
+int annoDiNascita = 1990;
+int eta = 2023 - annoDiNascita;
+System.out.println("Età: " + eta);
