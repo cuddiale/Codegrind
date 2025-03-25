5d7130a1b744cb6c9efd6f69e3286343042ddd92
cuddiale <alessiocuddretto@hotmail.com>
2025-03-25 17:38:31 +0100

    update

---
+++ linguaggi di programmazione/C++/Costanti/.cpp
@@ -0,0 +1,10 @@
+// Definire una costante stringa e stamparne il valore.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    const string COSTANTE_STRINGA = "Ciao";
+    cout << "Il valore della costante è: " << COSTANTE_STRINGA << endl;
+    return 0;
+}
---
+++ linguaggi di programmazione/C++/Costanti/2.cpp
@@ -0,0 +1,10 @@
+// Definire una costante di tipo float e stamparne il valore.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    const float COSTANTE_FLOAT = 3.14;
+    cout << "Il valore della costante è: " << COSTANTE_FLOAT << endl;
+    return 0;
+}
