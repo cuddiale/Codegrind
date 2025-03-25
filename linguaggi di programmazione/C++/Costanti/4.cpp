4e27ec6b7570b1005f1799820e75ca105e4f3f2a
cuddiale <alessiocuddretto@hotmail.com>
2025-03-25 17:44:24 +0100

    update

---
+++ linguaggi di programmazione/C++/Costanti/4.cpp
@@ -0,0 +1,11 @@
+// Definire una costante di tipo double e utilizzarla in un'operazione matematica.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double COSTANTE_DOUBLE = 2.71828;
+    double risultato = COSTANTE_DOUBLE * 2;
+    cout << "Il risultato dell'operazione è: " << risultato << endl;
+    return 0;
+}
---
+++ linguaggi di programmazione/C++/Costanti/5.cpp
@@ -0,0 +1,10 @@
+// Definire una costante di tipo char e stamparne il valore.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    const char COSTANTE_CHAR = 'A';
+    cout << "Il valore della costante è: " << COSTANTE_CHAR << endl;
+    return 0;
+}
