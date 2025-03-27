82d4b7f56f86960119bad3998f6c140d93da5b46
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-03-27 09:41:58 +0100

    update

---
+++ linguaggi di programmazione/C++/Operatori Assegnazione Composta/2.cpp
@@ -0,0 +1,10 @@
+// Utilizzare l'operatore `-=` per sottrarre un valore a una variabile.
+
+#include <iostream>
+
+int main() {
+    int a = 10;
+    a -= 3;
+    std::cout << "a -= 3: " << a << std::endl;
+    return 0;
+}
---
+++ linguaggi di programmazione/C++/Operatori Assegnazione Composta/3.cpp
@@ -0,0 +1,10 @@
+// Utilizzare l'operatore `*=` per moltiplicare una variabile per un valore.
+
+#include <iostream>
+
+int main() {
+    int a = 10;
+    a *= 2;
+    std::cout << "a *= 2: " << a << std::endl;
+    return 0;
+}
