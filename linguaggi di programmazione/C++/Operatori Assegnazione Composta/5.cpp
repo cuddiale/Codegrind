338346b792ec2ebbc3906d907227604bee6b533e
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-03-27 09:51:06 +0100

    update

---
+++ linguaggi di programmazione/C++/Operatori Assegnazione Composta/5.cpp
@@ -0,0 +1,10 @@
+// Utilizzare l'operatore `%=` per ottenere il resto della divisione di una variabile per un valore.
+
+#include <iostream>
+
+int main() {
+    int a = 10;
+    a %= 3;
+    std::cout << "a %= 3: " << a << std::endl;
+    return 0;
+}
