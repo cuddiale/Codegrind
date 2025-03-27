321aa2c408e65de26ec4d23f72fed5230a885ea0
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-03-27 10:29:54 +0100

    update

---
+++ linguaggi di programmazione/C++/Ciclo for/3.cpp
@@ -0,0 +1,12 @@
+// Utilizza un ciclo for per stampare i multipli di 3 da 1 a 100.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 3; i <= 100; i += 3) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
