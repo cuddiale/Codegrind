f7a5797b2b1797278886f6fd2f5e5a5c47e216c1
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-04-01 16:46:24 +0200

    update

--- linguaggi di programmazione/C++/Ereditarietà/4.cpp
+++ linguaggi di programmazione/C++/Ereditarietà/4.cpp
@@ -1,2 +1,34 @@
 // Crea una classe derivata Automobile dalla classe Veicolo con un membro privato modello e un metodo pubblico impostaModello che imposta il modello dell'automobile.
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Veicolo {
+protected:
+    string marca;
+    int annoProduzione;
+
+public:
+    void impostaMarca(string newMarca) {
+        marca = newMarca;
+    }
+};
+
+class Automobile : public Veicolo {
+private:
+    string modello;
+
+public:
+    void impostaModello(string newModello) {
+        modello = newModello;
+    }
+};
+
+int main() {
+    Automobile a;
+    a.impostaMarca("Fiat");
+    a.impostaModello("Panda");
+
+    return 0;
+}
