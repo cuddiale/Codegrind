ee03b0b99803b1246b0a5c7353541e2c702e61f6
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-04-10 17:51:15 +0200

    update

---
+++ linguaggi di programmazione/C/Variabili/10.c
@@ -0,0 +1,6 @@
+// Scambiare il valore di due variabili intere utilizzando una variabile temporanea.
+
+int a = 5, b = 10, temp;
+temp = a;
+a = b;
+b = temp;
--- linguaggi di programmazione/C/Variabili/9.c
+++ linguaggi di programmazione/C/Variabili/9.c
@@ -1,3 +1,4 @@
 Dichiarare una costante chiamata "PI" e assegnarle il valore 3.14159.
 
 const float PI = 3.14159;
+
