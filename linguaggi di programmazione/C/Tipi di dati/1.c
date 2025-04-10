0175497ea1251e8be2072b11d9cd0f22daa6f4e0
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-04-10 17:52:24 +0200

    update

---
+++ linguaggi di programmazione/C/Tipi di dati/1.c
@@ -0,0 +1,3 @@
+// Dichiarare una variabile intera chiamata "numero" e assegnarle il valore 5.
+
+int numero = 5;
---
+++ linguaggi di programmazione/C/Tipi di dati/2.c
@@ -0,0 +1,3 @@
+// Dichiarare una variabile float chiamata "pi" e assegnarle il valore 3.14.
+
+float pi = 3.14;
