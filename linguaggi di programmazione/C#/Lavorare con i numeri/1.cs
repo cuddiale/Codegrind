19648ee3f088401f7f9c78ae68ef3fe96fc650ef
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-04-22 10:41:15 +0200

    update

---
+++ linguaggi di programmazione/C#/Lavorare con i numeri/1.cs
@@ -0,0 +1,6 @@
+// Scrivi un programma che dichiara due variabili numeriche e ne calcola la somma. Stampa il risultato a schermo.
+
+int numero1 = 10;
+int numero2 = 5;
+int somma = numero1 + numero2;
+Console.WriteLine("La somma è: " + somma);
