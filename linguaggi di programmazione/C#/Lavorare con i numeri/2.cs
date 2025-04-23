f4e53dc108a49a34bcebb27d9027cfe3133f105d
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-04-23 16:39:00 +0200

    create

---
+++ linguaggi di programmazione/C#/Lavorare con i numeri/2.cs
@@ -0,0 +1,6 @@
+// Scrivi un programma che dichiara due variabili numeriche e ne calcola la differenza. Stampa il risultato a schermo.
+
+int numero1 = 10;
+int numero2 = 5;
+int differenza = numero1 - numero2;
+Console.WriteLine("La differenza è: " + differenza);
