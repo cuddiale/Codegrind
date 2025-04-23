c24839ad6a8ec3b3ce80a4f385d55c1f3870385c
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-04-23 17:18:00 +0200

    update

---
+++ linguaggi di programmazione/C#/Operatori di comparazione/1.cs
@@ -0,0 +1,6 @@
+// Scrivi un programma che dichiara due variabili numeriche e utilizza l'operatore di confronto per determinare se sono uguali. Stampa il risultato a schermo.
+
+int numero1 = 10;
+int numero2 = 5;
+bool sonoUguali = numero1 == numero2;
+Console.WriteLine("I numeri sono uguali? " + sonoUguali);
