87c87fd23100f2b9bc96aaae07b6bbd9f376f7e1
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-04-24 09:55:35 +0200

    create

---
+++ linguaggi di programmazione/C#/Operatori di comparazione/2.cs
@@ -0,0 +1,6 @@
+// Scrivi un programma che dichiara due variabili di tipo stringa e utilizza l'operatore di confronto per determinare se sono uguali. Stampa il risultato a schermo.
+
+string stringa1 = "ciao";
+string stringa2 = "ciao";
+bool sonoUguali = stringa1 == stringa2;
+Console.WriteLine("Le stringhe sono uguali? " + sonoUguali);
