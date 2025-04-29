8e2635f5506c877d5dd5cc6ce678c539027171a4
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-04-29 16:41:41 +0200

    update

--- linguaggi di programmazione/C#/Ciclo While e Do While/4.cs
+++ linguaggi di programmazione/C#/Ciclo While e Do While/4.cs
@@ -1,2 +1,12 @@
 // Scrivi un programma che accetti un numero intero positivo da tastiera e utilizzi un ciclo 'while' per calcolarne il fattoriale.
 
+Console.Write("Inserisci un numero: ");
+int numero = int.Parse(Console.ReadLine());
+int fattoriale = 1;
+int i = 1;
+while (i <= numero)
+{
+    fattoriale *= i;
+    i++;
+}
+Console.WriteLine("Il fattoriale di " + numero + " è: " + fattoriale);
