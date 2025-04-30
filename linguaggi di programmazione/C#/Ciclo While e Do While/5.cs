ccd510d3a58dd56802f9a954f9316063fa3f8bca
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-04-30 17:18:47 +0200

    create

---
+++ linguaggi di programmazione/C#/Ciclo While e Do While/5.cs
@@ -0,0 +1,8 @@
+// Scrivi un programma che utilizzi un ciclo 'do while' per stampare i numeri da 10 a 1 in ordine decrescente.
+
+int numero = 10;
+do
+{
+    Console.WriteLine(numero);
+    numero--;
+} while (numero >= 1);
