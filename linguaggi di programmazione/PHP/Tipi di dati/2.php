b68292af6c6ed9164d299827d700978ded12d866
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-05-08 12:09:48 +0200

    update

---
+++ linguaggi di programmazione/PHP/Tipi di dati/2.php
@@ -0,0 +1,7 @@
+<!-- Dichiarare una variabile float chiamata "prezzo" e assegnarle il valore 9.99. Convertire la variabile in intero e stampare il risultato. -->
+
+<?php
+$prezzo = 9.99;
+$prezzo_intero = (int)$prezzo;
+echo $prezzo_intero;
+?>
