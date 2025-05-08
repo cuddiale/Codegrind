0a7b1536e05192482965b59054c24f0bf0c98788
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-05-08 12:17:51 +0200

    update

---
+++ linguaggi di programmazione/PHP/Tipi di dati/10.php
@@ -0,0 +1,8 @@
+<!-- Dichiarare una variabile float chiamata "peso" e assegnarle il valore 62.5. Convertire la variabile in intero e sottrarre il valore 10. Stampare il risultato. -->
+
+<?php
+$peso = 62.5;
+$peso_intero = (int)$peso;
+$risultato = $peso_intero - 10;
+echo $risultato;
+?>
