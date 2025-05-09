70ac08c6347e2ebcea68dd820d070a1c0073180e
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-05-09 12:36:31 +0200

    update

---
+++ linguaggi di programmazione/PHP/Lavorare con le stringhe/1.php
@@ -0,0 +1,10 @@
+<!-- Esercizio: Scrivi un codice PHP che stampi la lunghezza di una stringa. -->
+
+<?php
+
+$stringa = "Ciao, sono una stringa!";
+$lunghezza = strlen($stringa);
+
+echo "La lunghezza della stringa è: " . $lunghezza;
+
+?>
---
+++ linguaggi di programmazione/PHP/Lavorare con le stringhe/2.php
@@ -0,0 +1,10 @@
+<!-- Scrivi un codice PHP che converte una stringa in maiuscolo. -->
+
+<?php
+
+$stringa = "ciao, sono una stringa!";
+$maiuscolo = strtoupper($stringa);
+
+echo $maiuscolo;
+
+?>
