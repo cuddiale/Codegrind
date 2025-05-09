abdcf67106971b30b97cbeafcdfcfad7dda23486
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-05-09 11:43:58 +0200

    update

---
+++ linguaggi di programmazione/PHP/Lavorare con i numeri/6.php
@@ -0,0 +1,7 @@
+<!-- Scrivere un programma PHP per calcolare il valore assoluto di un numero e stampare il risultato. -->
+
+<?php
+$numero = -15;
+$risultato = abs($numero);
+echo "Il valore assoluto è: " . $risultato;
+?>
