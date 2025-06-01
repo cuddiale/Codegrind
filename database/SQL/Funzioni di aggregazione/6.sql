dc32f98da224a4067bf237355987071304e5b059
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-06-01 12:28:19 +0200

    update

---
+++ database/SQL/Funzioni di aggregazione/6.sql
@@ -0,0 +1,4 @@
+-- Ottieni la quantità totale di prodotti venduti.
+
+SELECT SUM(Quantita) AS QuantitaTotale
+FROM DettaglioOrdine;
---
+++ database/SQL/Funzioni di aggregazione/7.sql
@@ -0,0 +1,4 @@
+-- Ottieni il prezzo medio dei prodotti forniti.
+
+SELECT AVG(Prezzo) AS PrezzoMedio
+FROM Prodotto;
